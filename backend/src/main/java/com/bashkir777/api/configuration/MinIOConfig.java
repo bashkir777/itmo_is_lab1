aa6b5ea9c77997f7f5e6ae215652252ec0963609
Supletsov Dmitriy <supletsovd@gmail.com>
2024-12-22 15:19:14 +0300

    basic minIO config added

---
+++ backend/src/main/java/com/bashkir777/api/configuration/MinIOConfig.java
@@ -0,0 +1,34 @@
+package com.bashkir777.api.configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinIOConfig {
+
+    @Value("${minIO.access-key}")
+    private String accessKey;
+
+    @Value("${minIO.secret-key}")
+    private String secretKey;
+
+    @Value("${minIO.endpoint}")
+    private String endpoint;
+
+    @Bean
+    public AmazonS3 minioClient() {
+
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, "us-east-1"))
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+}
